851fcbcf3e8a725eb522bc017ea5b9d9f384048b
Giovanni Collazo <hello@gcollazo.com>
2015-05-27 17:36:16 -0400

    Sorted installs

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 
 echo "Installing utils..."
-brew install tree
 brew install nmap
 brew install readline
+brew install tree
 brew install watchman
 
 echo "Installing Cask..."
@@ -23,25 +23,24 @@ brew tap caskroom/fonts
 brew cask install font-inconsolata
 
 echo "Installing OS X Apps..."
-brew cask install spotify
-brew cask install transmission
-brew cask install paparazzi
-brew cask install viscosity
-brew cask install gpgtools
-brew cask install mou
-brew cask install skype
-
-brew cask install iterm2
-brew cask install heroku-toolbelt
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install rowanj-gitx
-brew cask install cyberduck
-brew cask install postgres
-brew cask install mongodb
-brew cask install sublime-text3
-brew cask install imageoptim
 brew cask install arq
+brew cask install cyberduck
+brew cask install gpgtools
+brew cask install heroku-toolbelt
+brew cask install imageoptim
+brew cask install iterm2
+brew cask install mongodb
+brew cask install mou
+brew cask install paparazzi
+brew cask install postgres
+brew cask install rowanj-gitx
+brew cask install skype
+brew cask install spotify
+brew cask install sublime-text3
+brew cask install transmission
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install viscosity
 
 echo "Installing development tools..."
 brew install bash-completion
@@ -64,10 +63,11 @@ echo "Installing global modules with npm..."
 npm install -g npm
 npm install -g bower
 npm install -g ember-cli
-npm install -g jshint
-npm install -g jscs
-npm install -g eslint
+
 npm install -g babel-eslint
+npm install -g eslint
+npm install -g jscs
+npm install -g jshint
 npm install -g nodemon
 npm install -g phantomjs
 
