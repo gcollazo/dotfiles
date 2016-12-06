d4eec6f2f2a043d17302100fd9785ee5d91e1eb0
Giovanni Collazo <hello@gcollazo.com>
2016-12-05 20:41:17 -0400

    Use a brewfile for hombrew deps

---
+++ Brewfile
@@ -0,0 +1,61 @@
+tap 'homebrew/core'
+tap 'homebrew/bundle'
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+
+brew 'jq'
+brew 'mas'
+brew 'nmap'
+brew 'tree'
+brew 'awscli'
+brew 'bash-completion'
+brew 'git'
+brew 'heroku-toolbelt'
+brew 'watchman'
+brew 'pyenv'
+brew 'pyenv-virtualenv'
+
+cask 'java'
+brew 'android-sdk'
+
+cask 'arduino'
+cask 'arq'
+cask 'atom'
+cask 'burp-suite'
+cask 'cyberduck'
+cask 'docker'
+cask 'font-inconsolata'
+cask 'genymotion'
+cask 'gitup'
+cask 'gpgtools'
+cask 'handbrake'
+cask 'imageoptim'
+cask 'iterm2'
+cask 'licecap'
+cask 'mongodb'
+cask 'ngrok'
+cask 'paparazzi'
+cask 'postgres'
+cask 'postman'
+cask 'robomongo'
+cask 'screenflow'
+cask 'sketch'
+cask 'skitch'
+cask 'slack'
+cask 'spotify'
+cask 'the-unarchiver'
+cask 'transmission'
+cask 'tunnelblick'
+cask 'virtualbox'
+cask 'vlc'
+cask 'webtorrent'
+cask 'wireshark'
+
+mas 'Pixelmator', id: 407963104
+mas 'Keynote', id: 409183694
+mas 'Pages', id: 409201541
+mas 'Numbers', id: 409203825
+mas 'Final Cut Pro', id: 424389933
+mas '1Password', id: 443987910
+mas 'Navicat Premium Essentials', id: 466416967
+mas 'Xcode', id: 497799835
--- install.sh
+++ install.sh
@@ -13,59 +13,9 @@ sudo xcode-select --install
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing utils..."
-brew install jq
-brew install mas
-brew install nmap
-brew install tree
-
-echo "Installing Cask..."
-brew tap caskroom/cask
-
-echo "Installing OS X Apps..."
-brew cask install arduino
-brew cask install arq
-brew cask install atom
-brew cask install burp-suite
-brew cask install cyberduck
-brew cask install docker
-brew cask install font-inconsolata
-brew cask install genymotion
-brew cask install gitup
-brew cask install gpgtools
-brew cask install handbrake
-brew cask install imageoptim
-brew cask install iterm2
-brew cask install licecap
-brew cask install mongodb
-brew cask install ngrok
-brew cask install paparazzi
-brew cask install postgres
-brew cask install postman
-brew cask install robomongo
-brew cask install screenflow
-brew cask install sketch
-brew cask install skitch
-brew cask install slack
-brew cask install spotify
-brew cask install the-unarchiver
-brew cask install transmission
-brew cask install tunnelblick
-brew cask install virtualbox
-brew cask install vlc
-brew cask install webtorrent
-brew cask install wireshark
-
-echo "Installing fonts..."
-brew tap caskroom/fonts
-brew cask install font-inconsolata
-
-echo "Installing development tools..."
-brew install awscli
-brew install bash-completion
-brew install git
-brew install heroku-toolbelt
-brew install watchman
+echo "Installing all of Homebrew..."
+brew tap Homebrew/bundle
+brew bundle
 
 # Install nvm
 echo "Installing nvm..."
@@ -98,8 +48,6 @@ npm install -g ios-deploy
 
 # Install Python
 echo "Installing Python..."
-brew install pyenv
-brew install pyenv-virtualenv
 pyenv install 2.7.12
 pyenv global 2.7.12
 source ~/.profile
@@ -107,7 +55,3 @@ source ~/.profile
 # Install Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby -- --ignore-dotfiles
-
-echo "Installing Android dev tools..."
-brew cask install java
-brew install android-sdk
