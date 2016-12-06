2dff9bd47576eb736e09eccb4eed5e6ffea5cf8d
Giovanni Collazo <hello@gcollazo.com>
2016-12-06 12:47:59 -0400

    Fix rvm install command

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ source ~/.profile
 # Install Ruby
 echo "Installing Ruby..."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby -- --ignore-dotfiles
+curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Atom packages
 echo "Installing Atom Packages..."
