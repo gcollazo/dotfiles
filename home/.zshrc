a35a2ad2dfc23752f158c6c4fdf5db5af61803cf
Giovanni Collazo <hello@gcollazo.com>
2023-02-18 14:41:33 -0400

    update for tmux

--- home/.zshrc
+++ home/.zshrc
@@ -48,5 +48,13 @@ export PATH="$GOPATH/bin:$PATH"
 # rust binaries
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Aliases
 source "$HOME/.aliases"
+
+
+source /Users/gcollazo/.docker/init-zsh.sh || true # Added by Docker Desktop
--- install.sh
+++ install.sh
@@ -13,19 +13,12 @@ sudo xcodebuild -license accept
 
 # Install Python
 echo "Installing Python..."
-pyenv install 3.10.0
-pyenv global 3.10.0
+pyenv install 3.11.1
+pyenv global 3.11.1
 
-# Install awsume
-pip install awsume
-
-# Install volta
-echo "Installing volta..."
-curl https://get.volta.sh | bash
-
-# Install latest release of node
-echo "Installing node..."
-volta install node
+# Configure keyboard
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
 
 echo
 echo "==> Done!"
