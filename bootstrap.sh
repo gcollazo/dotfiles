1b29d5badd819f1db0e1182e4a99458b7d59c626
Giovanni Collazo <hello@gcollazo.com>
2023-12-02 15:40:56 -0400

    install xcode cli from script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,21 @@ set -euf -o pipefail
 
 echo "==> Bootstrapping..."
 
+# Only run if the tools are not installed yet
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing..."
+  in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  touch ${in_progress}
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    tail -n 1 | sed 's/^[^C]* //')
+  softwareupdate -i "$PROD" --verbose;
+  rm ${in_progress}
+else
+  echo "Xcode CLI tools found. Skipping..."
+fi
+
 # Check if git is installed
 if ! command -v git >/dev/null; then
   echo "Git is not installed. Please install git first. xcode-select --install"
